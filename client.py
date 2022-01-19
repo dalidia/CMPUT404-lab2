5a1ef935d9a669fd2a24f023b750b455bbe2c1be
Lidia Ataupillco <ataupill@ualberta.ca>
2022-01-18 23:55:19 -0700

    added extra info about client and echo_server

--- client.py
+++ client.py
@@ -1,24 +1,41 @@
 import socket
 
-HOST = "localhost"
+HOST = "www.google.com"
 PORT = 80
-PAYLOAD = 'GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n'
+PAYLOAD = f'GET / HTTP/1.1\r\nHost: {HOST}\r\n\r\n'
 BUFFER_SIZE = 4096
 
-
 def main():
+    # creates a new socket
     try:
         s = socket.socket(socket.AF_INET)
-        s.connect((HOST, PORT))
-        s.send(PAYLOAD.encode())
-        # TODO: keeps checking for data
-        s.shutdown(socket.SHUT_WR)
+        print("Socket was created")
+    except OSError as e:
+        print("There was an error creating the socket", e)
+        return
+
+    # connect the socket
+    address = (HOST, PORT)
+    s.connect(address)
+    print(f"Socket connect to {HOST}")
+
+    try:
+        s.sendall(PAYLOAD.encode())
+    except socket.error:
+        print("There was an error sending the information")
+        return
+    
+    s.shutdown(socket.SHUT_WR)
+
+    all_data = b''
+    # check for incoming data greater than the buffer size
+    while True:
         data = s.recv(BUFFER_SIZE)
-        print("x", data)
-        # TODO: change exception
-    except Exception as e:
-        print("There was an error", e)
-    finally:
-        s.close()
+        if not data:
+            break
+        all_data += data
+
+    print(all_data)    
+    s.close()
 
 main()
--- echo_server.py
+++ echo_server.py
@@ -1,28 +1,38 @@
 import socket
+import time
 
 HOST = ''
 PORT = 8001
 BUFFER_SIZE = 1024
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # creates a new socket
+    try:
+        s = socket.socket(socket.AF_INET)
+        print("Socket was created")
+    except OSError as e:
+        print("There was an error creating the socket", e)
+        return
+
     # to send input to the terminal
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-    s.bind((HOST, PORT))
+    # bind socket to address
+    address = (HOST, PORT)
+    s.bind(address)
 
-    # allows a backlog of 2 requests
+    # allows a backlog of 2 connections
     s.listen(2)
-    print("listening")
+    print("Socket is listening...")
 
-    # Make socket to constantly listen to requests
+    # Make socket to constantly listen to connections
     while True:
         conn, address = s.accept()
         print("Connected by ", address)
 
         data_received = conn.recv(BUFFER_SIZE)
-        conn.send(data_received)
-        print("DATA", data_received)
+        time.sleep(0.5)
+        conn.sendall(data_received)
         conn.close()
-    
+
 
 main()
